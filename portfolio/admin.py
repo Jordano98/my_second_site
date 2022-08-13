622f650dd98ebe4151a37d35d0f12a946a9963a5
Jordano98 <mohammad.dehghan218@gmail.com>
2022-08-13 18:18:25 +0430

    completion of portfolio app modelation

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from portfolio.models import Pro_Category,Pro_Gallery,Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('pro_name','client','project_date','project_url','title','created_date')
+    list_filter = ('pro_name','project_date')
+    search_fields = ['title','content']
+
+admin.site.register(Project, ProjectAdmin)
+
+admin.site.register(Pro_Category)
+admin.site.register(Pro_Gallery)
+
 # Register your models here.
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+class Pro_Category(models.Model) :
+    name=models.CharField(max_length=225)
+
+    def __str__(self) :
+        return self.name
+    class Meta:
+        verbose_name_plural='categories'
+
+
+class Pro_Gallery(models.Model):
+    image = models.ImageField(upload_to='portfolio/', default='./portfolio-1.jpg')
+
+    class Meta:
+        verbose_name_plural='gallery'
+
+class Project(models.Model):
+    pro_name=models.CharField(max_length=255)
+    client=models.CharField(max_length=255)
+    project_date=models.DateTimeField(null=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    category = models.ManyToManyField(Pro_Category)
+    gallery = models.ManyToManyField(Pro_Gallery)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    login_require = models.BooleanField(default=False)
+    project_url=models.URLField(max_length=150)  
 # Create your models here.
