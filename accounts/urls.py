f96177d77c1118166c52d76136e5ee694a4aba8a
Jordano98 <mohammad.dehghan218@gmail.com>
2022-08-22 16:09:16 +0430

    forgot password part is settled

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+#from django.contrib.auth import views as auth_views
 
 app_name = "accounts"   
 
@@ -8,4 +9,8 @@ urlpatterns = [
     path("register", views.register_request, name="register"),
     path('login',views.login_view,name='login'),
     path('logout',views.logout_view,name='logout'),
+    path("password_reset", views.password_reset_request, name="password_reset"),
+    #path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    #path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name='password_reset_confirm'),
+    #path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'), 
 ]
--- my_second_site/urls.py
+++ my_second_site/urls.py
@@ -17,13 +17,17 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('website.urls')),
     path('blog/',include('blog.urls')),
     path('portfolio/',include('portfolio.urls')),
     path('accounts/',include('accounts.urls')),
+
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'), 
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
